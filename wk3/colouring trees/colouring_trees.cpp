fa42b92b99991363053f8f6111453f55d3a2d658
Dawn0247 <david@pbsit.com.au>
2024-10-04 15:23:36 +1000

    ps3 C

--- wk3/colouring trees/colouring_trees.cpp
+++ wk3/colouring trees/colouring_trees.cpp
@@ -4,18 +4,64 @@
 using namespace std;
 
 #define ll long long 
-const int MAXN = 100;
+const int MAXN = 101;
 int N, M, K;
 ll arr[MAXN];
-ll dp[1 << MAXN][MAXN];
-ll bonuses[MAXN][MAXN];
+ll paint[MAXN][MAXN];
+ll dp[MAXN][MAXN][MAXN];
 
 int main() {
     cin.tie(nullptr);
     cin.sync_with_stdio(false);
-    cin >> N >> M >> K;8
+    cin >> N >> M >> K;
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
+    } 
+
+    // paint[i][j] = cost paint tree i into color j
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> paint[i][j];
+        }
     }
+    
+
+    // dp: min cost for 
+    // i: first i trees
+    // j: colour of the i'th tree
+    // k: number of beauty groups
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            for (int k = 0; k < K; k++) {
+                if (!arr[i]) {
+                    for (int )
+
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << ' ';
+    } cout << '\n';
+
+    // paint[i][j] = cost paint tree i into color j
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << paint[i][j] << ' ';
+        } cout << '\n';
+    }
+
+
 
 }
