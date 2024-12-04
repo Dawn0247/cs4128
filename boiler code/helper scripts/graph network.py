63d07885c0426c6e75fa55e39fb82bce19d012d1
David Wu <david@pbsit.com.au>
2024-12-04 16:48:39 +1100

    helper scripts

--- boiler code/helper scripts/graph network.py
+++ boiler code/helper scripts/graph network.py
@@ -1,23 +1,23 @@
 import networkx as nx
-e = """0 1 10
-0 2 10
-1 2 2
-1 3 4
-1 4 8
-2 4 9
-3 5 10
-4 3 6
-4 5 10
+import matplotlib.pyplot as plt
+l = input()
+n, m = [int(x) for x in l.split()]
+e = []
+minn = 1e18
+maxn = 0
+for i in range(m):
+    u,v,w = [int(x) for x in input().split()]
+    minn = min(minn, v, u)
+    maxn = max(maxn, u, v)
+    e.append([u,v,w])
 
-"""
 G = nx.DiGraph()
-G.add_nodes_from(range(1, 7))
-for l in e.strip().split('\n'):
-    l = l.split()
-    G.add_edge(int(l[0]), int(l[1]), weight=int(l[2]))
+G.add_nodes_from(range(minn, maxn+1))
+for u,v,w in e:
+    G.add_edge(u, v, weight=w)
 
 pos=nx.circular_layout(G)
 nx.draw(G,pos, with_labels = True)
 labels = nx.get_edge_attributes(G,'weight')
 nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-
+plt.show()
--- boiler code/helper scripts/plot points.py
+++ boiler code/helper scripts/plot points.py
@@ -1,1 +1,20 @@
 import matplotlib.pyplot as plt
+n = int(input())
+pts = []
+xs = []
+ys = []
+ids = []
+
+for i in range(n):
+    x,y = [int(i) for i in input().split()]
+    ids.append(i)
+    xs.append(x)
+    ys.append(y)
+
+plt.scatter(xs, ys)
+
+for i, txt in enumerate(ids):
+    plt.annotate(txt, (xs[i], ys[i]))
+
+plt.show()
+
